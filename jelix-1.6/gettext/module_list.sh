e12d52daf9d606c15549b23ff082246c931269d6
Laurent Jouanneau <dev@ljouanneau.com>
2021-02-26 17:46:40 +0100

    Fix: move sl_SL to sl_SI

--- jelix-1.6/gettext/module_list.sh
+++ jelix-1.6/gettext/module_list.sh
@@ -3,10 +3,10 @@
 export MODULES="jelix master_admin jacl2 jacl2db jacl2db_admin jauthdb_admin"
 
 # all locales that are available into transifex.
-export AVAILABLE_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL sk_SK"
+export AVAILABLE_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SI sk_SK"
 
 # locales that are almost complete and so, that should be pushed to the project.
-export OFFICAL_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL sk_SK"
+export OFFICAL_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SI sk_SK"
 
 export LOCALES_BRANCH=jelix-1.6
 
--- modules/gettext/module_list.sh
+++ modules/gettext/module_list.sh
@@ -3,10 +3,10 @@
 export MODULES="jcommunity"
 
 # all locales that are available into transifex.
-export AVAILABLE_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL sk_SK"
+export AVAILABLE_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SI sk_SK"
 
 # locales that are almost complete and so, that should be pushed to the project.
-export OFFICAL_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SL sk_SK"
+export OFFICAL_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SI sk_SK"
 
 export LOCALES_BRANCH=modules
 
