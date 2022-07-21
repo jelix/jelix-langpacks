7fda40c26c9abda16cf6a9432d96a4e1c954fbe3
Laurent Jouanneau <dev@ljouanneau.com>
2022-07-21 16:22:15 +0200

    Locales for Jelix 1.8

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@
 build:
 	./build_jelix_packages.sh 1.6
 	./build_jelix_packages.sh 1.7
+	./build_jelix_packages.sh 1.8
 	./build_module_packages.sh jcommunity
 	./build_module_packages.sh ldapdao
 	./build_module_packages.sh multiauth
@@ -12,6 +13,7 @@ build:
 deploy:
 	jelix_upload_dir.sh _dist/jelix-1.6/ "$(CI_DEPLOY_USER)@$(CI_DEPLOY_SERVER)" langpacks/jelix-1.6
 	jelix_upload_dir.sh _dist/jelix-1.7/ "$(CI_DEPLOY_USER)@$(CI_DEPLOY_SERVER)" langpacks/jelix-1.7
+	jelix_upload_dir.sh _dist/jelix-1.8/ "$(CI_DEPLOY_USER)@$(CI_DEPLOY_SERVER)" langpacks/jelix-1.8
 	jelix_upload_dir.sh _dist/jcommunity/ "$(CI_DEPLOY_USER)@$(CI_DEPLOY_SERVER)" langpacks/jcommunity
 	jelix_upload_dir.sh _dist/ldapdao/ "$(CI_DEPLOY_USER)@$(CI_DEPLOY_SERVER)" langpacks/ldapdao
 	jelix_upload_dir.sh _dist/multiauth/ "$(CI_DEPLOY_USER)@$(CI_DEPLOY_SERVER)" langpacks/multiauth
---
+++ jelix-1.8/gettext/module_list.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+export MODULES="jelix master_admin jacl2 jacl2db jacl2db_admin jauthdb_admin"
+
+# all locales that are available into transifex.
+export AVAILABLE_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT ja_JP nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SI sk_SK uk_UA"
+export AVAILABLE_SHORT_LOCALES="cs de el es eu fi fr gl hu_HU it ja nl no pl_PL pt_BR pt ro ru sv_SE sl sk uk_UA"
+
+# locales that are almost complete and so, that should be pushed to the project.
+# in Jelix, we want the long language code (ex: ja_JP). If the translator registered
+# only into the short code, indicate the code as : short code/long code (ex: ja/ja_JP)
+export OFFICIAL_LOCALES="cs_CZ de_DE el_GR es_ES eu_ES fi_FI fr_FR gl_ES hu_HU it_IT ja_JP nl_NL no_NO pl_PL pt_BR pt_PT ro_RO ru_RU sv_SE sl_SI sk_SK uk_UA"
+
+export LOCALES_BRANCH=jelix-1.8
+
